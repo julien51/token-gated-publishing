3fb2d02031ea792ac6df23b60f5b5a475d42efe6
Julien Genestoux <julien.genestoux@gmail.com>
2022-02-10 11:58:14 -0500

    updated profile pic

--- src/components/bio.js
+++ src/components/bio.js
@@ -36,7 +36,7 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.png"
+        src="https://raw.githubusercontent.com/unlock-protocol/unlock/master/design/brand/PNG/Unlock-LogoMark-Circle-Border.png"
         width={50}
         height={50}
         quality={95}
